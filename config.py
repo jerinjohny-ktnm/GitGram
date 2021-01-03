380b6737fb968da83c5882a19ecd924bb9860d58
Jerin Johny <71542613+jerinjohny-ktnm@users.noreply.github.com>
2021-01-03 16:18:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # Your Bot token in the quotes taken from @BotFather
-BOT_TOKEN = "1158822364:AAEgnhKPvoTbPU5KnO8iMYqQfuF_XcxbBUE"
+BOT_TOKEN = "1481808581:AAHdClyJ3U4r3Tnwz6ArOHkNflwdb9OFKLI"
 
 # Project Name of your Project
-PROJECT_NAME = "GitGram"
+PROJECT_NAME = "Github Bot"
 
 # If you forked the source code, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
+GIT_REPO_URL = "https://github.com/jerinjohny-ktnm/GitGram/"
